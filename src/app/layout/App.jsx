c879d0cb205a03238aeed4d1b2bf574fe64c167a
Matt Givney <mgivney@gmail.com>
2020-02-28 22:00:57 -0500

    nav and layout

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,20 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import EventDashboard from '../../features/event/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/navBar/NavBar';
+import { Container } from 'semantic-ui-react';
 
 class App extends Component{
   render() {
     return (
-      <div className="App">
-        <h1>Re-vents</h1>
-      </div>
-    ) ;
+      <Fragment>
+      <NavBar />
+      <Container className="main">
+        <EventDashboard />
+      </Container>
+      </Fragment>
+    );
+    
+
   }
 }
 
